0bda976a014fcb1330aa396dd3767ca073c9aacb
gaurav gupta <anantgupta007>
2026-02-19 22:21:12 +0530

    update income tax

--- 19_feb/lab1/2.incometax.py
+++ 19_feb/lab1/2.incometax.py
@@ -6,18 +6,21 @@
 # Additionally, if the person is a senior citizen (age ≥ 60), increase the exemption limit to ₹3,00,000.
 
 
+
 age = int(input("Enter your age: "))
 income = float(input("Enter your annual income: "))
 if age >= 60:
     ex_limit = 300000
 else:    
     ex_limit = 250000
-    if income <= ex_limit:   
-        tax = 0
-    elif income <= 500000:   
-        tax = (income - ex_limit) * (5/100)
-    elif income <= 1000000:   
-        tax = (income - 500000) * (2/100) + (500000 - ex_limit) * (5/100)
-    else:    
-        tax = (income - 1000000) * (3/100) + (1000000 - 500000) * 0.20 + (500000 - ex_limit) * 0.05
+
+if income <= ex_limit:   
+    tax = 0
+elif income <= 500000:   
+    tax = (income - ex_limit) * (5/100)
+elif income <= 1000000:   
+    tax = (income - 500000) * (20/100) + (500000 - ex_limit) * (5/100)
+else:    
+    tax = (income - 1000000) * (30/100) + (1000000 - 500000) * 0.20 + (500000 - ex_limit) * 0.05
+
 print("Your income tax is: ₹", tax)
